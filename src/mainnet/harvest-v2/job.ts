f27765e4436826b73aa9595772926be7fa00f9fd
0xng <87835144+0xng@users.noreply.github.com>
2022-01-18 19:44:21 -0300

    feat: adding jobs (#1)

--- src/mainnet/harvest-v2/job.ts
+++ src/mainnet/harvest-v2/job.ts
@@ -1,8 +1,11 @@
-import { createForks, GanacheFork, Job, JobWorkableGroup, makeid, prelog, TransactionError } from '@keep3r-network/cli-utils';
-import { getGoerliSdk } from '../../eth-sdk-build';
+import { createForks, GanacheFork, getStealthHash, Job, JobWorkableGroup, makeid, prelog, TransactionError } from '@keep3r-network/cli-utils';
+import { getMainnetSdk } from '../../eth-sdk-build';
 import metadata from './metadata.json';
 
+// create an array that holds the expected errors to make them more readable
 const expectedErrors: string[] = ['V2Keep3rJob::work:not-workable', '!authorized', '!healthcheck'];
+
+// establish the maximum amount of strategies per fork
 const maxStrategiesPerFork = 5;
 
 const getWorkableTxs: Job['getWorkableTxs'] = async (args) => {
@@ -12,16 +15,17 @@ const getWorkableTxs: Job['getWorkableTxs'] = async (args) => {
     block: args.advancedBlock,
     logId: makeid(5),
   };
+
   const logConsole = prelog(logMetadata);
 
   logConsole.log(`Trying to work`);
 
   // setup job with default fork provider
-	const signer = args.fork.ethersProvider.getSigner(args.keeperAddress);
-	const { jobStrategies: defaultJob } = getGoerliSdk(signer);
+  const signer = args.fork.ethersProvider.getSigner(args.keeperAddress);
+  const { harvestJob: job } = getMainnetSdk(signer);
 
   // get strategies to work
-  const strategies: string[] = args.retryId ? [args.retryId] : await defaultJob.jobs();
+  const strategies: string[] = args.retryId ? [args.retryId] : await job.strategies();
   logConsole.log(args.retryId ? `Retrying strategy` : `Simulating ${strategies.length} strategies`);
 
   // create needed forks in order to work in parallel
@@ -31,16 +35,13 @@ const getWorkableTxs: Job['getWorkableTxs'] = async (args) => {
 
   // for each fork
   const workPromises = forks.map(async (fork, forkIndex) => {
-
-    // setup job using fork provider
+    // setup job and stealth relayer using fork provider
     const signer = fork.ethersProvider.getSigner(args.keeperAddress);
-    const { jobStrategies: forkJob } = getGoerliSdk(signer);
-
+    const { harvestJob: job, stealthRelayer } = getMainnetSdk(signer);
     const forkStrategies = strategies.slice(forkIndex * maxStrategiesPerFork, forkIndex * maxStrategiesPerFork + maxStrategiesPerFork);
 
     // for each strategy
     for (const [index, strategy] of forkStrategies.entries()) {
-
       // setup logs for strategy
       const strategyIndex = forkIndex * maxStrategiesPerFork + index;
       const strategyLogId = `${logMetadata.logId}-${makeid(5)}`;
@@ -52,15 +53,20 @@ const getWorkableTxs: Job['getWorkableTxs'] = async (args) => {
         continue;
       }
 
+      // encode function data and get stealth hash
+      const workData: string = job.interface.encodeFunctionData('work', [strategy]);
+      const stealthHash: string = getStealthHash();
+
       try {
         // check if strategy is workable
-        await forkJob.callStatic.execute(strategy, {
+        await stealthRelayer.callStatic.execute(job.address, workData, stealthHash, args.advancedBlock, {
           blockTag: args.advancedBlock,
         });
+
         strategyConsole.log(`Strategy #${strategyIndex} is workable`, { strategy });
 
         // create work tx
-        const tx = await forkJob.populateTransaction.execute(strategy, {
+        const tx = await stealthRelayer.populateTransaction.execute(job.address, workData, stealthHash, args.targetBlock + index, {
           nonce: args.keeperNonce,
           gasLimit: 5_000_000,
           type: 2,
@@ -78,8 +84,7 @@ const getWorkableTxs: Job['getWorkableTxs'] = async (args) => {
           workableGroups,
           correlationId: strategy,
         });
-
-      } catch (err: any) {
+      } catch (err: unknown) {
         // handle error logs
         const isExpectedError = expectedErrors.find((expectedError) => {
           return (err as TransactionError).message?.includes(expectedError);
@@ -88,7 +93,7 @@ const getWorkableTxs: Job['getWorkableTxs'] = async (args) => {
         if (!isExpectedError) {
           strategyConsole.warn(`Strategy #${strategyIndex} failed with unknown error`, {
             strategy,
-            message: err.message,
+            message: (err as Error).message,
           });
         } else {
           strategyConsole.log(`Strategy #${strategyIndex} is not workable`, { strategy });
