b39fe6395c80548bda796f61d9e9d2afe55bd170
0xGorilla <84932007+0xGorilla@users.noreply.github.com>
2022-10-05 15:29:30 +0200

    feat: upgrade jobs (#5)

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   outputPath: 'src/eth-sdk-build',
   contracts: {
     mainnet: {
-      harvestJob: '0xe6dd4b94b0143142e6d7ef3110029c1dce8215cb',
-      tendJob: '0xcd7f72f12c4b87dabd31d3aa478a1381150c32b3',
+      harvestJob: '0x220a85bCd2212ab0b27EFd0de8b5e03175f0adee',
+      tendJob: '0xdeE991cbF8527A33E84a2aAb8a65d68D5D591bAa',
       stealthRelayer: '0x0a61c2146A7800bdC278833F21EBf56Cd660EE2a',
     },
   },
